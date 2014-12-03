e85ae40bb35f546aa319c0ae017a980bc95d8ac5
Marcus Stelke <stelke@informatik.uni-bremen.de>
2014-12-03 17:31:20 +0100

    Navigation Drawer implementiert

---
+++ Babyfon/src/babyfon/fragment/SetupFragment.java
@@ -0,0 +1,24 @@
+package babyfon.fragment;
+
+import babyfon.activity.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SetupFragment extends Fragment {
+
+	public SetupFragment() {
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_setup, container, false);
+
+		return rootView;
+	}
+}
